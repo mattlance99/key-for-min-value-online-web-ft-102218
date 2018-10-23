3cce9fab02870e2764577a79b3b012eecc98f3a0
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-23 17:06:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
-  if name_hash = {}
+  if name_hash == {}
     return nil
   else
       min_value = name_hash.first[1]
